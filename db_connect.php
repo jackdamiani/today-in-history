34cac3adb73dd9fe8f1534812f8d2198eab165bc
Jack <jdamiani227@gmail.com>
2024-05-27 16:54:28 -0400

    testing php

---
+++ db_connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost"; // or the host provided by Hostinger
+$username = "u880862300_tih_user_stats";
+$password = "m?6Y|/&VexQ";
+$dbname = "u880862300_user_stats";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
