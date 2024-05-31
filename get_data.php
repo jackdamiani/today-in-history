75252406917f9ff31a75de88b78969bb779fd4b8
Jack <jdamiani227@gmail.com>
2024-05-30 20:28:04 -0400

    oops

--- get_data.php
+++ get_data.php
@@ -14,7 +14,6 @@
 
     // Get today's date
     $today = date('Y-m-d');
-    echo $today;
 
     // Prepare and execute the SQL query
     $sql = "SELECT num_guesses FROM test WHERE passed = 1 AND date = ?";
