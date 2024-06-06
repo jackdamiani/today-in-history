1f5c820574475445ef212340e4303bab011de2b1
Jack <jdamiani227@gmail.com>
2024-06-05 22:59:22 -0400

    debug11

--- results.php
+++ results.php
@@ -794,7 +794,7 @@
 
         .scoreHistogram {
             display: block;
-            z-index: -1;
+            z-index: 10000;
             position: relative;
             top: 0;
             left: 0;
