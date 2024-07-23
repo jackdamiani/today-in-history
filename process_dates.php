6141ab6f7aab44fa92d9946aff823d6a2c68cf04
Jack <jdamiani227@gmail.com>
2024-07-22 21:47:25 -0400

    debug7

--- process_dates.php
+++ process_dates.php
@@ -12,8 +12,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $clientDate = date('Y-m-d', strtotime($clientDate));
         $anotherDate = date('Y-m-d', strtotime($anotherDate));
 
-        // echo $clientDate;
-        // echo $anotherDate;
+        echo $clientDate;
+        echo $anotherDate;
 
         $servername = "localhost"; // or the host provided by Hostinger
         $username = "u880862300_tih_user_stats";
