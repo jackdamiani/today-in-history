5b66969bbc30c0e91af7db5f7d1c5ea769097e00
Jack <jdamiani227@gmail.com>
2024-08-11 00:10:50 -0400

    events! celeb

--- celeb/cevents.js
+++ celeb/cevents.js
@@ -1,4 +1,196 @@
 var today_in_history = {
     "0802": {"1951": "Joe Lynn Turner, singer", "1970": "Kevin Smith, filmaker", "1992": "Charli XCX, singer"},
     "0803": {"1963": "James Hetfield", "1977": "Tom Brady, football player", "1984": "Ryan Lochte, swimmer"},
+
+
+
+    "0901": {"1946": "Barry Gibb, Singer (Bee Gees)", "1950": "Phil McGraw, Television Host (Dr. Phil)", "1996": "Zendaya, Actress/Singer"},
+    "0902": {
+        "1948": "Terry Bradshaw, Football Player and Analyst (FOX)",
+        "1964": "Keanu Reeves, Actor (Matrix, John Wick)",
+        "1966": "Salma Hayek, Actress (Grown Ups, Magic Mike)"
+    },
+    "0903": {
+        "1965": "Charlie Sheen, Actor (Two and a Half Men)",
+        "1969": "Noah Baumbach, Filmmaker (Kicking and Screaming, While We're Young)",
+        "1975": "Redfoo, Musician (LMFAO)"
+    },
+    "0904": {
+        "1931": "Mitzi Gaynor, Actress (Golden Girl)",
+        "1960": "Damon Wayans, Actor/Comedian (Beverly Hills Cop, Bulletproof)",
+        "1981": "Beyoncé, Singer (Single Ladies, If I Were a Boy)"
+    },
+    "0905": {
+        "1946": "Freddie Mercury, Singer (Queen)",
+        "1951": "Michael Keaton, Actor (Batman)",
+        "1989": "Kat Graham, Actress/Singer (Lizzy McGuire, The Vampire Diaries)"
+    },
+    "0906": {
+        "1943": "Roger Waters, Musician (Pink Floyd)",
+        "1962": "Chris Christie, Politician (New Jersey)",
+        "1971": "Dolores O'Riordan, Singer (The Cranberries)"        
+    },
+    "0907": {
+        "1936": "Buddy Holly, Singer (That'll Be the Day, Peggy Sue)",
+        "1967": "Leslie Jones, Comedian (Saturday Night Live)",
+        "1987": "Evan Rachel Wood, Actress (Westworld, Thirteen)"
+    },
+    "0908": {
+        "1979": "P!nk, Singer (Just Give Me a Reason, So What)",
+        "1981": "Jonathan Taylor Thomas, Actor (Home Improvement, The Lion King)",
+        "1989": "Avicii, DJ/Producer (Wake Me Up, Levels)"
+    },
+    "0909": {
+        "1960": "Hugh Grant, Actor (Notting Hill, Love Actually)",
+        "1966": "Adam Sandler, Actor/Comedian (Happy Gilmore, Saturday Night Live)",
+        "1975": "Michael Bublé, Singer (Haven't Met You Yet, Feeling Good)"
+    },
+    "0910": {
+        "1945": "Jose Feliciano, Singer (Feliz Navidad, Light My Fire)",
+        "1968": "Guy Ritchie, Director (Snatch, Sherlock Holmes)",
+        "1964": "Chris Columbus, Director (Home Alone, Harry Potter and the Sorcerer's Stone)"
+    },
+    "0911": {
+        "1940": "Brian De Palma, Director (Scarface, The Untouchables)",
+        "1977": "Ludacris, Rapper/Actor (Fast & Furious series, Word of Mouf)",
+        "1987": "Tyler Hoechlin, Actor (Teen Wolf, Superman & Lois)"
+    },
+    "0912": {
+        "1973": "Paul Walker, Actor (Fast & Furious series, Into the Blue)",
+        "1977": "2 Chainz, Rapper (It's a Vibe, Birthday Song)",
+        "1986": "Emmy Rossum, Actress/Singer (Shameless, Phantom of the Opera)"
+    },
+    "0913": {
+        "1916": "Roald Dahl, Author (Charlie and the Chocolate Factory, Matilda)",
+        "1969": "Tyler Perry, Actor/Producer (Madea, Tyler Perry's House of Payne)",
+        "1993": "Niall Horan, Singer (One Direction, Flicker)"
+    },
+    "0914": {
+        "1972": "Nas, Rapper (Illmatic, God's Son)",
+        "1983": "Amy Winehouse, Singer (Back to Black, Rehab)",
+        "1989": "Jimmy Butler, Basketball Player (NBA All-Star, Miami Heat)"
+    },
+    "0915": {
+        "1961": "Dan Marino, Football Player (Miami Dolphins, NFL Hall of Fame)",
+        "1981": "Ben Schwartz, Actor/Comedian (Parks and Recreation)",
+        "1984": "Prince Harry, Duke of Sussex (British Royal Family)"
+    },
+    "0916": {
+        "1968": "Marc Anthony, Singer (Vivir Mi Vida, I Need to Know)",
+        "1971": "Amy Poehler, Actress/Comedian (Parks and Recreation, Saturday Night Live)",
+        "1992": "Nick Jonas, Singer/Actor (Jonas Brothers, Jumanji: Welcome to the Jungle)"
+    },
+    "0917": {
+        "1962": "Baz Luhrmann, Director (Moulin Rouge!, The Great Gatsby)",
+        "1972": "Bobby Lee, Comedian (MADtv, TigerBelly Podcast)",
+        "1974": "Rasheed Wallace, Basketball Player (NBA Champion, Detroit Pistons)"
+    },
+    "0918": {
+        "1961": "James Gandolfini, Actor (The Sopranos, The Mexican)",
+        "1971": "Jada Pinkett Smith, Actress (The Matrix, Red Table Talk)",
+        "1975": "Jason Sudeikis, Actor/Comedian (Ted Lasso, Saturday Night Live)",
+    },
+    "0919": {
+        "1974": "Jimmy Fallon, Comedian/TV Host (The Tonight Show, Saturday Night Live)",
+        "1976": "Alison Sweeney, Actress (Days of Our Lives, The Biggest Loser)",
+        "1983": "Kevin Zegers, Actor (Gossip Girl, The Mortal Instruments: City of Bones)"
+    },
+    "0920": {
+        "1948": "George R. R. Martin, Author (A Song of Ice and Fire, Game of Thrones)",
+        "1975": "Juan Pablo Montoya, Racing Driver (Formula 1, Indy 500)",
+        "1976": "Jon Bernthal, Actor (The Punisher, The Walking Dead)",
+    },
+    "0921": {
+        "1934": "Leonard Cohen, Singer/Songwriter (Hallelujah, Suzanne)",
+        "1950": "Bill Murray (SNL, Groundhog Day)",
+        "1972": "Liam Gallagher (Oasis, Wonderwall)"
+    },
+    "0922": {
+        "1960": "Scott Baio, Actor (Happy Days, Charles in Charge)",
+        "1982": "Billie Piper, Singer/Actress (Doctor Who, Because We Want To)",
+        "1987": "Tom Felton, Actor (Harry Potter series)"
+    },
+    "0923": {
+        "1949": "Bruce Springsteen, American singer-songwriter (Born in the U.S.A.)",
+        "1978": "Anthony Mackie, American actor (The Falcon and the Winter Soldier)",
+        "1985": "Hasan Minhaj, American comedian and actor (Patriot Act)",
+    },
+    "0924": {
+        "1962": "Nia Vardalos, Canadian actress and screenwriter (My Big Fat Greek Wedding)",
+        "1985": "Jessica Lucas, Canadian actress (Gotham)",
+        "1993": "Ben Platt, Actor (Pitch Perfect)"
+    },
+    "0925": {
+        "1968": "Will Smith, actor and rapper (The Fresh Prince of Bel-Air)",
+        "1976": "Chauncey Billups, NBA player and coach",
+        "1984": "Catherine Zeta-Jones, actress (Chicago)"
+    },
+    "0926": {
+        "1948": "Olivia Newton-John, Singer and Actress (Grease)",
+        "1981": "Serena Williams, Tennis player",
+        "1985": "Talulah Riley, Actress (Pride and Prejudice, Westworld)"
+    },
+    "0927": {
+        "1947": "Meat Loaf, Singer and Actor (Bat Out of Hell)",
+        "1972": "Gwyneth Paltrow, Actress (Shakespeare in Love, Iron Man)",
+        "1984": "Avril Lavigne, Singer-Songwriter (Sk8ter Boi, What the Hell)"
+    },
+    "0928": {
+        "1968": "Naomi Watts, Actress (The Ring, Divergent)",
+        "1987": "Hilary Duff, Actress and Singer (Cheaper by the Dozen, Cadet Kelly)",
+        "1999": "Olivia Jade Giannulli, Youtuber"
+    },
+    "0929": {
+        "1957": "Andrew Dice Clay, Comedian and Actor (A Star is Born)",
+        "1988": "Kevin Durant, NBA Basketball Player",
+        "1995": "Sasha Lane, Actress (Loki)"
+    },
+    "0930": {
+        "1964": "Monica Bellucci, Italian actress, Model (The Matrix Reloaded)",
+        "1982": "Lacey Chabert, American actress (Mean Girls)",
+        "1984": "T-Pain, Rapper and Singer (Buy U a Drank, Can't Believe It)"
+    },
+    "1001": {
+        "1963": "Mark McGwire, MLB Baseball Player",
+        "1969": "Zach Galifianakis, Actor and Comedian (The Hangover)",
+        "1983": "Brie Larson, Actress (Captain Marvel)"
+    },
+    "1002": {
+        "1951": "Sting, English musician (The Police)",
+        "1970": "Kelly Ripa, Actress and Talk Show Host (Live!)",
+        "2002": "Jacob Sartorius, Singer and Influencer (Sweatshirt)"
+    },
+    "1003": {
+        "1962": "Tommy Lee, American musician (Mötley Crüe)",
+        "1969": "Gwen Stefani, Singer (No Doubt, Sweet Escape)",
+        "1984": "Ashlee Simpson, Singer (Pieces of Me)",
+    },
+    "1004": {
+        "1988": "Derrick Rose, NBA Basketball Player",
+        "1989": "Dakota Johnson, Actress (Fifty Shades of Grey)",
+        "1992": "Melissa Benoist, Actress (Glee, Supergirl)"
+    },
+    "1005": {
+        "1958": "Neil deGrasse Tyson, Astrophysicist",
+        "1975": "Kate Winslet, Actress (Titanic)",
+        "1983": "Jesse Eisenberg, Actor (The Social Network, Now You See Me)"
+    },
+    "1006": {
+        "1963": "Elisabeth Shue, Actress (The Karate Kid, Leaving Las Vegas)",
+        "1978": "Ioan Gruffudd, Actor (Fantastic Four)",
+        "2011": "Ryan Kaji, Youtuber (Ryan's World)"
+    }
+
+
+
+
+
+    
+
+
+
+
+
+
+
 }
