fb4ca1080d2dfe82c9e2ca40263fe89f9797348c
Jack <jdamiani227@gmail.com>
2024-08-11 21:48:13 -0400

    color update

--- celeb/cevents.js
+++ celeb/cevents.js
@@ -1,7 +1,7 @@
 var today_in_history = {
     "0802": {"1951": "Joe Lynn Turner, singer", "1970": "Kevin Smith, filmaker", "1992": "Charli XCX, singer"},
     "0803": {"1963": "James Hetfield", "1977": "Tom Brady, football player", "1984": "Ryan Lochte, swimmer"},
-
+    "0811": {"1111": "test", "1112": "test", "1113": "test"},
 
 
     "0901": {
--- sport/sevents.js
+++ sport/sevents.js
@@ -1,4 +1,5 @@
 var today_in_history = {
     "0802": {"1951": "Joe Lynn Turner, singer", "1970": "Kevin Smith, filmaker", "1992": "Charli XCX, singer"},
     "0803": {"1963": "James Hetfield", "1977": "Tom Brady, football player", "1984": "Ryan Lochte, swimmer"},
+    "0811": {"1963": "James Hetfield", "1977": "Tom Brady, football player", "1984": "Ryan Lochte, swimmer"}
 }
